4ad78e606db7df930c640c1faee5813a9d0d2ad4
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-11-27 21:17:06 +0530

    feat:added forgot password api integration

--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,6 @@ export const register = (userDetails) => {
 export const login = (data) => {
     return http.post("/login", data);
 };
+export const forgot = (data) => {
+    return http.post("/forgotPassword", data);
+  };
